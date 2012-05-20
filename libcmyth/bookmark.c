9096ffad822a795521ed099a13674fe4406f8e69
David Teirney <david@teirney.net>
2012-05-20 12:04:00 +1200

    Formatting test for bookmark.c
    Uses Lindent script from linux kernel scripts that applies the linux kernel coding style.

--- libcmyth/bookmark.c
+++ libcmyth/bookmark.c
@@ -23,7 +23,6 @@
 #include <errno.h>
 #include <cmyth_local.h>
 
-
 long long cmyth_get_bookmark(cmyth_conn_t conn, cmyth_proginfo_t prog)
 {
 	char *buf;
@@ -39,44 +38,44 @@ long long cmyth_get_bookmark(cmyth_conn_t conn, cmyth_proginfo_t prog)
 	if (!buf) {
 		return -ENOMEM;
 	}
-	sprintf(buf,"%s %ld %s","QUERY_BOOKMARK",prog->proginfo_chanId,
+	sprintf(buf, "%s %ld %s", "QUERY_BOOKMARK", prog->proginfo_chanId,
 		start_ts_dt);
 	pthread_mutex_lock(&mutex);
-	if ((err = cmyth_send_message(conn,buf)) < 0) {
+	if ((err = cmyth_send_message(conn, buf)) < 0) {
 		cmyth_dbg(CMYTH_DBG_ERROR,
-			"%s: cmyth_send_message() failed (%d)\n",
-			__FUNCTION__, err);
+			  "%s: cmyth_send_message() failed (%d)\n",
+			  __FUNCTION__, err);
 		ret = err;
 		goto out;
 	}
 	count = cmyth_rcv_length(conn);
 	if (count < 0) {
 		cmyth_dbg(CMYTH_DBG_ERROR,
-			"%s: cmyth_rcv_length() failed (%d)\n",
-			__FUNCTION__, count);
+			  "%s: cmyth_rcv_length() failed (%d)\n",
+			  __FUNCTION__, count);
 		ret = count;
 		goto out;
 	}
-	if ((r=cmyth_rcv_long_long(conn, &err, &ll, count)) < 0) {
+	if ((r = cmyth_rcv_long_long(conn, &err, &ll, count)) < 0) {
 		cmyth_dbg(CMYTH_DBG_ERROR,
-			"%s: cmyth_rcv_longlong() failed (%d)\n",
-			__FUNCTION__, r);
+			  "%s: cmyth_rcv_longlong() failed (%d)\n",
+			  __FUNCTION__, r);
 		ret = err;
 		goto out;
 	}
-
 	ret = ll;
-   out:
+out:
 	pthread_mutex_unlock(&mutex);
 	return ret;
 }
-	
-int cmyth_set_bookmark(cmyth_conn_t conn, cmyth_proginfo_t prog, long long bookmark)
+
+int
+cmyth_set_bookmark(cmyth_conn_t conn, cmyth_proginfo_t prog, long long bookmark)
 {
 	char *buf;
 	unsigned int len = CMYTH_TIMESTAMP_LEN + CMYTH_LONGLONG_LEN + 18;
 	char resultstr[3];
-	int r,err;
+	int r, err;
 	int ret;
 	int count;
 	char start_ts_dt[CMYTH_TIMESTAMP_LEN + 1];
@@ -85,26 +84,28 @@ int cmyth_set_bookmark(cmyth_conn_t conn, cmyth_proginfo_t prog, long long bookm
 	if (!buf) {
 		return -ENOMEM;
 	}
-	sprintf(buf,"%s %ld %s %lld %lld","SET_BOOKMARK",prog->proginfo_chanId,
-		start_ts_dt, bookmark >> 32,(bookmark & 0xffffffff));
+	sprintf(buf, "%s %ld %s %lld %lld", "SET_BOOKMARK",
+		prog->proginfo_chanId, start_ts_dt, bookmark >> 32,
+		(bookmark & 0xffffffff));
 	pthread_mutex_lock(&mutex);
-	if ((err = cmyth_send_message(conn,buf)) < 0) {
+	if ((err = cmyth_send_message(conn, buf)) < 0) {
 		cmyth_dbg(CMYTH_DBG_ERROR,
-			"%s: cmyth_send_message() failed (%d)\n",
-			__FUNCTION__, err);
+			  "%s: cmyth_send_message() failed (%d)\n",
+			  __FUNCTION__, err);
 		ret = err;
 		goto out;
 	}
 	count = cmyth_rcv_length(conn);
-	if ((r=cmyth_rcv_string(conn,&err,resultstr,sizeof(resultstr),count)) < 0) {
+	if ((r = cmyth_rcv_string(conn, &err, resultstr, sizeof(resultstr),
+				  count)) < 0) {
 		cmyth_dbg(CMYTH_DBG_ERROR,
-			"%s: cmyth_rcv_string() failed (%d)\n",
-			__FUNCTION__, count);
+			  "%s: cmyth_rcv_string() failed (%d)\n",
+			  __FUNCTION__, count);
 		ret = count;
 		goto out;
 	}
-	ret = (strncmp(resultstr,"OK",2) == 0);
-   out:
+	ret = (strncmp(resultstr, "OK", 2) == 0);
+out:
 	pthread_mutex_unlock(&mutex);
 	return ret;
 }
